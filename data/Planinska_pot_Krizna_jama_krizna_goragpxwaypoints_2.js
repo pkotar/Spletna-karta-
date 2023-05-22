86fd6944b98f253685ea7f43cff0fd5fc2ebe813
pkotar <134282906+pkotar@users.noreply.github.com>
2023-05-22 13:30:52 +0200

    Add files via upload

---
+++ data/Planinska_pot_Krizna_jama_krizna_goragpxwaypoints_2.js
@@ -0,0 +1,1 @@
+var json_Planinska_pot_Krizna_jama_krizna_goragpxwaypoints_2 = {"type":"FeatureCollection","name":"Planinska_pot_Krizna_jama_krizna_goragpxwaypoints_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
